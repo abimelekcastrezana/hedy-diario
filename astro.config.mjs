875f62468fc6bc588429b0e10a7932d8b7c13b0a
Hedy <hedy@users.noreply.github.com>
2026-09-14 06:44:38 +0000

    Footer simplificado, quitar archivos de ejemplo del blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://abimelekcastrezana.github.io',
-	base: '/hedy-diario',
+	base: '/hedy-diario/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
